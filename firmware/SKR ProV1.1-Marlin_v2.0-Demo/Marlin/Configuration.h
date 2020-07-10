a7f1d721b419845c49fa273c7401c07dbf0066ba
Marcy-Hi <66909776+Marcy-Hi@users.noreply.github.com>
2020-07-10 10:40:26 +0200

    runot

--- firmware/SKR ProV1.1-Marlin_v2.0-Demo/Marlin/Configuration.h
+++ firmware/SKR ProV1.1-Marlin_v2.0-Demo/Marlin/Configuration.h
@@ -1147,6 +1147,8 @@
   #define FIL_RUNOUT_PULLUP          // Use internal pullup for filament runout pins.
   //#define FIL_RUNOUT_PULLDOWN      // Use internal pulldown for filament runout pins.
 
+  #define FIL_RUNOUT_PIN PE15
+
   // Set one or more commands to execute on filament runout.
   // (After 'M412 H' Marlin will ask the host to handle the process.)
   #define FILAMENT_RUNOUT_SCRIPT "M600"
